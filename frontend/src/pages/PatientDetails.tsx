ffaea0e9c4e48d609601534c2af72a08c480c442
ahmednaser111 <ahmednaser7707@gmail.com>
2025-12-26 20:06:02 +0200

    fix: remove unused variable linting rule in PatientDetails component

--- frontend/src/pages/PatientDetails.tsx
+++ frontend/src/pages/PatientDetails.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useParams, useNavigate } from "react-router-dom";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { patientsApi } from "@/api/patients";
